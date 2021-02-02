67755f0cad88149008f85bba15d66ab8ca337eeb
Uri Kutner <urikutner@gmail.com>
2021-02-02 12:56:54 +0200

    add compositions to curve peek

---
+++ src/geometry/curve-peek/curve-peak.compositions.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { CurvePeek } from './curve-peak';
+
+export const Preview = () => (
+	<CurvePeek filling="cloud" style={{ width: '160px', height: '100px' }} />
+);
