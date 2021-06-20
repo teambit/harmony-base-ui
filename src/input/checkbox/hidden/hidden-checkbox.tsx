0ab53b6ada557e040c5d44c1c026fc038980a17a
Josh Kuttler <yoshuakuttler@gmail.com>
2021-06-20 18:12:42 +0300

    tag and export

--- src/input/checkbox/hidden/hidden-checkbox.tsx
+++ src/input/checkbox/hidden/hidden-checkbox.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './hidden-checkbox.module.scss';
 
-export type HiddenCheckboxProps = {} & React.InputHTMLAttributes<HTMLInputElement>;
+export type HiddenCheckboxProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 /** Hidden but interactive checkbox */
 export function HiddenCheckbox({ className, type = 'checkbox', ...rest }: HiddenCheckboxProps) {
