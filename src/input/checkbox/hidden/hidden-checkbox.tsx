2e60160f28baa78a7f635ec31ff6aa3406b93e9c
Josh Kuttler <yoshuakuttler@gmail.com>
2021-06-20 18:04:09 +0300

    update hidden checbox to use type from prop

--- src/input/checkbox/hidden/hidden-checkbox.tsx
+++ src/input/checkbox/hidden/hidden-checkbox.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './hidden-checkbox.module.scss';
 
-export type HiddenCheckboxProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'>;
+export type HiddenCheckboxProps = {} & React.InputHTMLAttributes<HTMLInputElement>;
 
 /** Hidden but interactive checkbox */
-export function HiddenCheckbox(props: HiddenCheckboxProps) {
-	return (
-		<input
-			{...props}
-			type="checkbox"
-			className={classnames(props.className, styles.hidden)}
-		/>
-	);
+export function HiddenCheckbox({ className, type = 'checkbox', ...rest }: HiddenCheckboxProps) {
+	return <input {...rest} type={type} className={classnames(className, styles.hidden)} />;
 }
