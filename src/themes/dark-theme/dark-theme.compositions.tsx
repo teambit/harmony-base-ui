b8fa7741cf211203b68430c27057b11de7c22e9c
Uri Kutner <urikutner@gmail.com>
2021-06-23 12:30:15 +0300

    extended dark-theme

--- src/themes/dark-theme/dark-theme.compositions.tsx
+++ src/themes/dark-theme/dark-theme.compositions.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { primaryPalette } from '@teambit/base-ui.theme.color-definition';
 import { darkMode } from './index';
 
-const colors = [
+import styles from './dark-theme.compositions.module.scss';
+
+const textColors = [
 	'--bit-text-color',
 	'--bit-text-color-heavy',
 	'--bit-text-color-light',
 	'--bit-text-inactive',
+];
+const borders = [
 	'--bit-border-color',
 	'--bit-border-color-heavy',
 	'--bit-border-color-light',
 	'--bit-border-color-lightest',
+];
+const accents = [
 	'--bit-accent-color',
 	'--bit-accent-heavy',
 	'--bit-accent-light',
@@ -18,9 +25,12 @@ const colors = [
 	'--bit-text-on-accent',
 	'--bit-accent-bg',
 	'--bit-accent-bg-heavy',
+];
+const backgrounds = [
+
 	'--bit-bg-color',
 	'--bit-bg-heavy',
-	'--bit-bg-heaviest',
+	'--bit-bg-heavy',
 	'--bit-bg-heaviest',
 	'--bit-bg-bedrock',
 	'--bit-bg-navigation',
@@ -29,6 +39,9 @@ const colors = [
 	'--bit-bg-tooltip',
 	'--bit-bg-tooltip-heavy',
 	'--bit-bg-dent',
+];
+
+const errors = [
 	'--bit-error-color',
 	'--bit-error-heavy',
 	'--bit-error-light',
@@ -36,12 +49,99 @@ const colors = [
 	'--bit-error-bg-heavy',
 ];
 
-export function Preview() {
+export function Texts() {
 	return (
-		<div className={darkMode} style={{ background: 'black', padding: 8, borderRadius: 4 }}>
-			{colors.map((x) => (
+		<div className={darkMode} style={{ background: '#0c0c0c', height: '100%', boxSizing: 'border-box', padding: 8, borderRadius: 4 }}>
+			{textColors.map((x) => (
 				<ColorBox colorName={x} />
 			))}
+		</div>
+	);
+}
+
+export function Accents() {
+	return (
+		<div className={darkMode} style={{ background: '#0c0c0c', height: '100%', boxSizing: 'border-box', padding: 8, borderRadius: 4 }}>
+			{accents.map((x) => (
+				<ColorBox colorName={x} />
+			))}
+		</div>
+	);
+}
+
+export function Borders() {
+	return (
+		<div className={darkMode} style={{ background: '#0c0c0c', height: '100%', boxSizing: 'border-box', padding: 8, borderRadius: 4 }}>
+			{borders.map((x) => (
+				<ColorBox colorName={x} />
+			))}
+		</div>
+	);
+}
+
+export function Backgrounds() {
+	return (
+		<div className={darkMode} style={{ background: '#0c0c0c', height: '100%', boxSizing: 'border-box', padding: 8, borderRadius: 4 }}>
+			{backgrounds.map((x) => (
+				<ColorBox colorName={x} />
+			))}
+		</div>
+	);
+}
+
+export function Errors() {
+	return (
+		<div className={darkMode} style={{ background: '#0c0c0c', height: '100%', boxSizing: 'border-box', padding: 8, borderRadius: 4 }}>
+			{errors.map((x) => (
+				<ColorBox colorName={x} />
+			))}
+			<div>
+				(may be deprecated in the future.
+				<br />
+				use accentColors.error instead)
+			</div>
+		</div>
+	);
+}
+
+// (property) JSX.IntrinsicElements.div: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+
+
+export function ThemeExample({ className, ...rest }: React.HTMLAttributes<HTMLDivElement>) {
+	const [isDark, setDark] = useState(true);
+
+	return (
+		<div className={isDark ? darkMode : primaryPalette} style={{ height: '100%' }}>
+			<div className={className + ' ' + styles.container} {...rest}>
+
+				<div className={styles.regular}>
+					<div className={styles.accentText}>accent text</div>
+					<div>regular content</div>
+					<button className={styles.btn} onClick={() => setDark(x => !x)}>toggle dark mode {isDark ? '🌙' : '☀️'}</button>
+				</div>
+
+				<br />
+
+				<div className={styles.bg}>
+					<div className={styles.accentText}>accent text</div>
+					<div>interactive card</div>
+					<button className={styles.btn}>accent colored box</button>
+				</div>
+
+				<br />
+
+				<div className={styles.accentBg}>
+					<div className={styles.accentText}>accent text</div>
+					<div>accent card</div>
+					<button className={styles.btn}>accent colored box</button>
+				</div>
+
+				<br />
+
+				<div className={styles.accentCard}>
+					<div>accent card variation 2</div>
+				</div>
+			</div>
 		</div>
 	);
 }
