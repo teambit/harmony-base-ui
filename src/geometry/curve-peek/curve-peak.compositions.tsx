90b013f6512a4fc3fc82e1548a10f536351dff50
Uri Kutner <urikutner@gmail.com>
2021-07-07 15:32:31 +0300

    use currentColor in curvePeek

--- src/geometry/curve-peek/curve-peak.compositions.tsx
+++ src/geometry/curve-peek/curve-peak.compositions.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
-import { CurvePeek } from './curve-peak';
+import { CurvePeek, CurveValley } from './curve-peak';
 
-export const Preview = () => (
-	<CurvePeek filling="cloud" style={{ width: '160px', height: '100px' }} />
+export const Peak = () => (
+	<CurvePeek style={{ width: '260px', height: '100px', color: '#ededed' }} />
 );
+
+export const Valley = () => (
+	<CurveValley style={{ width: '260px', height: '100px', color: '#ededed' }} />
+);
+
+Valley.canvas = {
+	backgroundColor: '#414141',
+};
+
+// export const White = () => (
+// 	<CurvePeek filling="cloud" style={{ width: '160px', height: '100px', color: '#ffffff', boxShadow:  }} />
+// );
+// White.canvas = {
+// 	background: '#0c0c0c',
+// };
+
+// export const Cloud = () => (
+// 	<CurvePeek filling="cloud" style={{ width: '160px', height: '100px', color: '#f6f6f6' }} />
+// );
--- src/geometry/curve-peek/curve-peak.tsx
+++ src/geometry/curve-peek/curve-peak.tsx
@@ -1,42 +1,40 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import fillingStyle from './filling.module.scss';
+import styles from './curve-peek.module.scss';
 
-type PossibleFilling = 'none' | 'white' | 'cloud';
-
-export type SemicircleProps = {
-	/** Select the color for the curve background. */
-	filling: PossibleFilling;
-} & React.SVGProps<SVGSVGElement>;
+export type SemicircleProps = React.SVGProps<SVGSVGElement>;
 
 /**
  * An SVG component shaped like a parabola. Useful for separating two sections.<br/>
  * Being an html element, this component is very light and don't require an additional network request.<br/>
- * 
- * The component comes with 2 predefined colors, 'white' and 'cloud'.<br/>
- * To use a custom color, set fillings to be "none", and apply the color with a css rule, e.g. `{ filling: '#7f1ae5' }`
- * @name CurvePeek
  */
 export function CurvePeek(props: SemicircleProps) {
-	const { className, filling, ...rest } = props;
+	const { className, ...rest } = props;
 
 	return (
 		<svg
 			{...rest}
 			viewBox="0 0 100 100"
 			preserveAspectRatio="none"
-			className={classNames(fillingStyle[filling] || undefined, className)}
+			className={classNames(styles.curvePeek, className)}
 		>
-			<path
-				d="
-				M 0,100
-				Q 50,-100 100,100"
-			/>
+			<path d="M 0,100 Q 50,-100 100,100" />
 		</svg>
 	);
 }
 
-CurvePeek.defaultProps = {
-	filling: 'white',
-};
+export function CurveValley(props: SemicircleProps) {
+	const { className, ...rest } = props;
+
+	return (
+		<svg
+			{...rest}
+			viewBox="0 0 100 100"
+			preserveAspectRatio="none"
+			className={classNames(styles.curvePeek, className)}
+		>
+			<path d="M 0,0 Q 50,200 100,0" />
+		</svg>
+	);
+}
