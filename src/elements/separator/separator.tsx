8b36352dcc0f92149e219afb0e681673ecbda2e0
Josh Kuttler <yoshuakuttler@gmail.com>
2021-10-07 14:12:23 +0300

    update separator component and update prettier config

--- src/elements/separator/separator.tsx
+++ src/elements/separator/separator.tsx
@@ -3,31 +3,27 @@ import classNames from 'classnames';
 import styles from './separator.module.scss';
 
 export type SeparatorProps = {
-	/**
-	 * set as vertical / horizontal separator
-	 */
-	layout: 'vertical' | 'horizontal';
+  /**
+   * set as vertical / horizontal separator
+   */
+  layout?: 'vertical' | 'horizontal';
 } & React.HTMLAttributes<HTMLDivElement>;
 
 /**
  * Separating line.
  *
  * Use these implicit CSS variables to style:
- * -   `--bit-border-color`
+ * -   `--bit-border-color-lightest`
  * @name Separator
  * @example
  * <Separator />
  */
-export function Separator({ layout, className, ...rest }: SeparatorProps) {
-	return (
-		<div
-			data-bit-id="teambit.base-ui/elements/separator"
-			className={classNames(styles.separator, styles[layout], className)}
-			{...rest}
-		/>
-	);
+export function Separator({ layout = 'horizontal', className, ...rest }: SeparatorProps) {
+  return (
+    <div
+      data-bit-id="teambit.base-ui/elements/separator"
+      className={classNames(styles.separator, styles[layout], className)}
+      {...rest}
+    />
+  );
 }
-
-Separator.defaultProps = {
-	layout: 'horizontal',
-};
