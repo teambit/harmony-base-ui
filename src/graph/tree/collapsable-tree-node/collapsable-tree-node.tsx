404279aede38e5b573f342fd3a3f28646de8fa46
Josh Kuttler <35892475+JoshK2@users.noreply.github.com>
2021-11-07 22:03:20 +0200

    create collapsable tree node component (#23)
    * create the component
    
    * fix pr
    
    * use TreeLayer
    
    * add tests
    
    * tag and export

---
+++ src/graph/tree/collapsable-tree-node/collapsable-tree-node.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode, HTMLAttributes } from 'react';
+import classNames from 'classnames';
+import AnimateHeight from 'react-animate-height';
+import { TreeLayer, TreeNodeProps } from '@teambit/base-ui.graph.tree.recursive-tree';
+import { indentClass, indentStyle } from '@teambit/base-ui.graph.tree.indent';
+import styles from './collapsable-tree-node.module.scss';
+
+export type CollapsableTreeNodeProps = {
+  /**
+   * The title to be rendered and to be clicked to open the content.
+   */
+  title: ReactNode;
+
+  /**
+   * If the content is open or not.
+   */
+  isOpen?: boolean;
+} & Omit<HTMLAttributes<HTMLDivElement>, 'title'> &
+  TreeNodeProps;
+
+export function CollapsableTreeNode({ title, isOpen = false, node, depth, className }: CollapsableTreeNodeProps) {
+  return (
+    <div className={className}>
+      {title}
+      {node.children && (
+        <AnimateHeight height={isOpen ? 'auto' : 0}>
+          <div className={classNames(indentClass, styles.childrenTree)} style={indentStyle(depth + 1)}>
+            <TreeLayer childNodes={node.children} depth={depth + 1} />
+          </div>
+        </AnimateHeight>
+      )}
+    </div>
+  );
+}
