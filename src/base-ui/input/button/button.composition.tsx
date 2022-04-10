60dedba3343f363c098bf65ac034f7c649e87b4f
Uri Kutner <urikutner@gmail.com>
2022-04-10 14:44:35 +0300

    leftovers

--- src/base-ui/input/button/button.composition.tsx
+++ src/base-ui/input/button/button.composition.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './button';
 
 export const BasicButton = () => (
-	<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-		<Button onClick={() => alert('on click')}>Click</Button>
-	</div>
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <Button onClick={() => alert('on click')}>Click</Button>
+  </div>
 );
+
+export const ButtonTest = () => {
+  const [on, setOn] = useState(true);
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      {on && <Button onClick={() => setOn((x) => !x)}>Click</Button>}
+    </div>
+  );
+};
